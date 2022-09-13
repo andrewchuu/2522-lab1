614c5398b197b7ba0d03883a87d2f90e4ba2650b
andrewchuu <andrewchu06@gmail.com>
2022-09-12 22:57:39 -0700

    fixed checkstyle errors - andrew

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,11 @@
 package org.comp2522;
 
+/**
+ * Main class that simply prints a hello world statement to test repo creation
+ * a successful push to git hub.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello COMP 2522 world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello COMP 2522 world!");
+  }
 }
